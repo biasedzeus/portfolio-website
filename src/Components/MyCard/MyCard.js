1c58100c58819eefc02ded9eb9e6d0fe46e42e36
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-13 17:32:12 +0530

    card added
    two cards added to show code and design

---
+++ src/Components/MyCard/MyCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MyCard = () => {
+    return (
+        <div className="myCard">
+
+            <div className="object-image"></div>
+            <div className="card-body"></div>
+            
+        </div>
+    )
+}
+
+export default MyCard
