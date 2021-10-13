8f91493684bd7cfdde40b03a411b719a7cc8fe0d
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-13 19:49:46 +0530

    card created with styling almost done

--- src/Components/MyCard/MyCard.js
+++ src/Components/MyCard/MyCard.js
@@ -1,14 +1,19 @@
+import "./MyCard.css"
 import React from 'react'
 
-const MyCard = () => {
+const MyCard = ({image,text}) => {
     return (
         <div className="myCard">
 
-            <div className="object-image"></div>
-            <div className="card-body"></div>
-            
+           
+            <div  className="card-body">
+                <p className="card-text">{text}</p></div>
+            <span id="logo">
+              <img className="card-image" src={image} alt=""/>
+
+            </span>
         </div>
     )
 }
 
-export default MyCard
+export default MyCard;
