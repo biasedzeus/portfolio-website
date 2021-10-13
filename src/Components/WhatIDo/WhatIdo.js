1682830e4d409ee16f6f9d84b00e00a26bea2a71
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-13 19:51:40 +0530

    (text) updated to scale with screen resposive
    later add responsive behaviour to Cards

--- src/Components/WhatIDo/WhatIdo.js
+++ src/Components/WhatIDo/WhatIdo.js
@@ -1,15 +1,24 @@
 import "./WhatIdo.css"
 import React from 'react'
 import MyCard from '../MyCard/MyCard'
+import ideLogo from "../../ide-final-logo.png"
+import penLogo from "../../pen-logo-final.png"
 
 const WhatIdo = () => {
+
+    const designText = "I value simple content structure,clean design patterns and ease UI .";
+    const codeText = "I value simple and clean code which is easy to maintain and to refactor ."
     return (
-        <div>
+        <div className="what-i-do-container">
             <div className="text-holder">
             <span className="first-line">What I Do? </span><span className="second-line"> Glad You Asked.</span>
 
             </div>
-          <MyCard/>
+            <div class="card-holder">
+          <MyCard image = {penLogo} text ={designText} />
+            <MyCard image={ideLogo} text ={codeText}/>
+            </div>
+          
         </div>
     )
 }
