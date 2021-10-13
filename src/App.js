aa1765c45304699925fcfa2a30966123312ccc92
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-13 17:35:24 +0530

    layout and CSS updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer';
 import Hero from './Components/Hero';
+import WhatIdo from './Components/WhatIDo/WhatIdo';
 
 function App() {
   return (
     <div>
       <Hero/>
+      <WhatIdo/>
       <Contact/>
       <Contact/>
       <Contact/>
