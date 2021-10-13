e57d1dd496dd855cb8d34577b11d8f2be41c020c
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-13 20:56:47 +0530

    projects and whatIdo comps added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer';
-import Hero from './Components/Hero';
+import Hero from './Hero/Hero';
 import WhatIdo from './Components/WhatIDo/WhatIdo';
+import Projects from './Projects/Projects';
 
 function App() {
   return (
     <div>
       <Hero/>
       <WhatIdo/>
+      <Projects/>
       <Contact/>
       <Contact/>
       <Contact/>
